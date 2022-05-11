710aff9e198d5cf346798d83fa71a20fa6f467fb
Mehti Sadixov <82047091+mexdi008@users.noreply.github.com>
2022-05-11 16:51:58 +0400

    schedule_task

---
+++ scheduleTask/app/Console/Kernel.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
+
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * Define the application's command schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
+
+
+    protected function schedule(Schedule $schedule)
+    {
+        //  $schedule->command('report:send')->everyMinute();
+        $schedule->call(function(){
+            Log::channel('last')->info('1 deqiqe');
+        })->everyMinute();
+    }
+
+    /**
+     * Register the commands for the application.
+     *
+     * @return void
+     */
+    protected function commands()
+    {
+        $this->load(__DIR__.'/Commands');
+
+        require base_path('routes/console.php');
+    }
+}
